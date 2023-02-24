934d5ab7005c3141015bbcf59014c1d5a0d4edea
mithu <mithu@hanmail.net>
2023-02-25 08:36:48 +0900

    test 03

--- demo/src/demo/test.java
+++ demo/src/demo/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		var str= "test OK";
+		var str= "test OK 1234";
 
 		System.out.println("test desu!!!! :"+str);
 
