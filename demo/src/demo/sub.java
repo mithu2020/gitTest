ae4d69eb8fe391348348124dc88514cb53c772dd
mithu <mithu@hanmail.net>
2023-06-18 07:16:15 +0900

    test111

--- demo/src/demo/sub.java
+++ demo/src/demo/sub.java
@@ -5,6 +5,7 @@ public class sub {
     public static void main(String[] args) {
         System.out.println("Sub Branch. new!!!");
         System.out.println("Master Branch.");
+        System.out.println("Master Branch 22222.");
     }
 
 }
