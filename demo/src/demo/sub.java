02dae76b5ebba73ff9d8f7cacecef3d23700835f
mithu2020 <65519145+mithu2020@users.noreply.github.com>
2023-06-24 21:38:37 +0900

    Update sub.java

--- demo/src/demo/sub.java
+++ demo/src/demo/sub.java
@@ -4,9 +4,9 @@ public class sub {
 
     public static void main(String[] args) {
         System.out.println("Sub Branch. new!!!");
-        System.out.println("Master Branch.");        
+        System.out.println("Master Branch.");
+        System.out.println("Master Branch 22222.");
         Console.log("Hello DelftStack");
-        Console.log("Hello DelftStack222");
         
     }
 
