f923e78cfb5276b6fdd7243450b17f70c4d2f994
mithu <mithu@hanmail.net>
2023-06-26 07:47:47 +0900

    from VSCODE

--- demo/src/demo/test2.java
+++ demo/src/demo/test2.java
@@ -5,9 +5,9 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		String str= "test2";
+		String str = "test2";
 
-		System.out.println("test2 desu2!!!! :"+str);
+		System.out.println("test2 desu2!!!!aaa:" + str);
 
 	}
 
