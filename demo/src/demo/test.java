254d09bf1380deb3703849e090c98f183d1d32c4
mithu <mithu@hanmail.net>
2024-02-01 10:04:06 +0900

    test update

--- demo/src/demo/test.java
+++ demo/src/demo/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		String str= "test OK 1234555";
+		String str= "test OK 1234555  today!!!";
 
 		System.out.println("test desu!!!!22333 :"+str);
 
